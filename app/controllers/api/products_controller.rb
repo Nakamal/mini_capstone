02de1bddddf842f0901ff4ac1d6f00a7f7644319
Nakamal <nickhaugland@gmail.com>
2018-08-28 20:28:12 -0500

    able to search for individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products_view.json.jbuilder"
   end
+
+  def first_product_action
+    @product = Product.first
+    render "first_product_view.json.jbuilder"
+  end
+
+  def second_product_action
+    @product = Product.find_by(id: 2)
+    render "second_product_view.json.jbuilder"
+  end
+
+  def last_product_action
+    @product = Product.last
+    render "last_product_view.json.jbuilder"
+  end
 end
---
+++ app/views/api/products/last_product_view.json.jbuilder
@@ -0,0 +1,6 @@
+json.id product.id
+json.name product.name
+json.price product.price
+json.description product.description
+json.image_url product.image_url
+json.attunement product.attunement

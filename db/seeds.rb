d56e740390e55339a3997169aeb1c80b93cab0cd
Nakamal <nickhaugland@gmail.com>
2018-08-30 23:17:33 -0500

    saved full description

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+data = [
+{
+"name" => "Luckstone",
+"price" => 175,
+"description" => "While this polished agate is on your person, you gain a +1 bonus to Ability Checks and Saving Throws.",
+"image_url" => "https://media-waterdeep.cursecdn.com/avatars/thumbnails/7/434/1000/1000/636284772336589393.jpeg ",
+"attunement" => "Required",
+"link" => "http://localhost:3000/api/products/1"
+},
+{
+"name" => "Immovable Rod",
+"price" => 50,
+"description" => "This flat iron rod has a button on one end. You can use an action to press the button, which causes the rod to become magically fixed in place. Until you or another creature uses an action to push the button again, the rod doesn't move, even if it is defying gravity. The rod can hold up to 8,000 pounds of weight. More weight causes the rod to deactivate and fall. A creature can use an action to make a DC 30 Strength check, moving the fixed rod up to 10 feet on a success",
+"image_url" => "http://www.talesfromthefireside.com/galleryitems/immovable-rod-0.jpg",
+"attunement" => "none",
+"link" => "http://localhost:3000/api/products/4"
+},
+{
+"name" => "Cube of Force",
+"price" => 25000,
+"description" => "This cube is about an inch across. Each face has a distinct marking on it that can be pressed. The cube starts with 36 Charges, and it regains 1d20 expended Charges daily at dawn. You can use an action to press one of the cube's faces, expending a number of Chargesbased on the chosen face, as shown in the Cube of Force Faces table. Each face has a different effect. If the cube has insufficient Charges remaining, nothing happens. Otherwise, a barrier of Invisible force springs into existence, forming a cube 15 feet on a side. The barrier is centered on you, moves with you, and lasts for 1 minute, until you use an action to press the cube's sixth face, or the cube runs out of Charges. You can change the barrier's effect by pressing a different face of the cube and expending the requisite number of Charges, resetting the Duration. If your Movement causes the barrier to come into contact with a solid object that can't pass through the cube, you can't move any closer to that object as long as the barrier remains. Cube of Force Faces:\r\nFace — Charges — Effect:1 — 1 — Gases, wind, and fog can't pass through the barrier. 2 — 2 — Nonliving matter can't pass through the barrier. Walls, floors, and ceilings can pass through at your discretion. 3 — 3 — Living matter can 't pass through the barrier. 4 — 4 — Spell effects can 't pass through the barrier. 5 — 5 — Nothing can pass through the barrier. Walls, floors, and ceilings can pass through at your discretion. 6 — 0 — The barrier deactivates. The cube loses Charges when the barrier is targeted by certain Spells or comes into contact with certain spell or magic item effects, as shown in the table below. Spell or item — Charges Lost: Disintegrate — 1d12, Horn of Blasting — 1d10, Passwall — 1d6, Prismatic Spray — 1d20, Wall of Fire — 1d4",
+"image_url" => "https://media-waterdeep.cursecdn.com/avatars/thumbnails/7/172/1000/1000/636284724072432859.jpeg",
+"attunement" => "required",
+"link" => "http://localhost:3000/api/products/5"
+},
+{
+"name" => "Ocean's Claim",
+"price" => 50000,
+"description" => "From Sea all life has come, to Sea all owe a debt, and into Sea all shall return. This massive hunk of metal, covered in algae, is perpetually cold and damp. Light and sound seem to fade when close to it, compounded further when wielding it. This magical weapon has a +2 bonus to attack and damage rolls. The Ocean's Claim functions as a anchor, with a chain giving it a reach of 20 feet. Any living creature brought to 0 HP by the Ocean's Claim has a puddle of water form beneath their body, before dragging them through the puddle into the ocean without leaving a trace. Their soul remains trapped in the sea as a ghost, instead of moving on to their normal afterlife. As an action with the Ocean's Claim, you can perform a pull attack. When you make a melee attack against a Large or smaller creature, and on a successful hit, you may pull them next to your location. When you use this effect, your attack deals cold damage instead of bludgeoning or piercing damage. You may use this effect once, and regain its usage after you finish a short or long rest, or when you reduce a living(not undead or construct) creature to 0 HP with this weapon.",
+"image_url" => "https://cdn.shopify.com/s/files/1/2802/7198/products/product-image-567229823_300x300.jpg?v=1527523546",
+"attunement" => "required",
+"link" => "http://localhost:3000/api/products/2"
+},
+{
+"name" => "Captain Jendora's Hat of Storms",
+"price" => 50000,
+"description" => "When attuned, the Storm Hat grants +2 to your Intelligence, as well as resistance to fire, cold and lightning damage. Also, once attuned, the Storm Hat begins to rain onto the head of the wearer. The intensity of the rain depends on how many charges the hat currently has, where 5 charges means torrential downpour and 0 charges means only a light mist. When attuned, the Storm Hat has 1d4 charges. While wearing this hat, as an action you can expend any number of charges to cast a spell from the list below with a spell level equal to the number of charges spent. DC 16. thunderclap, thunderwave, fog cloud, water breathing, tidal wave, watery sphere, maelstrom. The hat is linked to the elemental plane of water. Each time you use the hat to cast a spell, roll a d20 with a DC of 1 + (the spells level). On a failure, a creature from the elemental plane of water will be sucked through the hat to your plane. (The number and type of creatures that appear is up to the DM.) Any creature with a size greater than huge may take longer to be pulled all the way through, or may be only partially pulled through (ie. a kraken's tentacles. (Again, this is up to the DM.)) The hat regains 1d4 expended charges daily at dawn up to a maximum of 5 charges. Curse. This hat is cursed, a fact that is revealed only when an Identify is cast on it, or you attune to it. Attuning to the hat curses you until you are targeted by a remove curse spell or similar magic. While cursed, the hat cannot be removed from your head.",
+"image_url" => "https://images-na.ssl-images-amazon.com/images/I/51jfArfQTuL._SX342_QL70_.jpg",
+"attunement" => "required",
+"link" => "http://localhost:3000/api/products/3"
+}
+]

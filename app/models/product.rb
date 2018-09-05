607a12e36e51c064bb9f22bdd54e89d072df2b9f
Nakamal <nickhaugland@gmail.com>
2018-09-04 19:11:23 -0500

    validations added in class, still no rarities added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { minimum: 10 }
+  validates :description, length: { maximum: 3500 }
+  validates :image_url, uniqueness: true
+  
+
+
   def is_discounted?
     price < 10
   end

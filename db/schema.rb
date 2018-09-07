bb884f08e6e31fd76971483345971c0d2ea364ca
Nakamal <nickhaugland@gmail.com>
2018-09-06 22:21:08 -0500

    image url attribute deleted, app currently working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_012054) do
+ActiveRecord::Schema.define(version: 2018_09_07_023159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2018_09_07_012054) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 7, scale: 2
-    t.string "image_url"
     t.text "description"
     t.string "rarity"
     t.string "attunement"

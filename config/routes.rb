a720ac7c787a2e65e1c0138780a32d717d7e54b1
Nakamal <nickhaugland@gmail.com>
2018-09-17 21:40:46 -0500

    added carted_products model and controller, currently working

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
     get "/orders" => "orders#index"
     post "/orders" => "orders#create"
 
+    get "/carted_products" => "carted_products#index"
+    post "/carted_products" => "carted_products#create"
+
     get "/products" => "products#index"
     post "/products" => "products#create"
     get "/products/:id" => "products#show"

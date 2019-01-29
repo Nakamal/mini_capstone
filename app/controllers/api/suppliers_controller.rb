c9913d4add2612cfd18b87973764aeebe1f4e69a
Nakamal <nickhaugland@gmail.com>
2019-01-29 10:02:26 -0600

    added created status in sessions controller

--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -1,3 +1,3 @@
 class Api::SuppliersController < ApplicationController
-  
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
         end
       end
     end
-  helper_method :current_user
+  helper_method :current_user 
 
   # def authenticate_user
   #   unless current_user
